9b13efccf622c060eff4893f2ef3bae4802acfc8
fabrezi <fkschon@gmail.com>
2023-01-05 12:53:51 -0500

    mo server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 7777
 const cors = require('cors')
 const path = require('path')
 
@@ -8,4 +8,8 @@ app.use(cors())
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
 })
